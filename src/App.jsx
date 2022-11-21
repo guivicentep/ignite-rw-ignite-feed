8ec986376f0609b3ad2ef533f8e6a6761d50c959
guivicentep <guivicente.gvp@hotmail.com>
2022-11-21 17:30:39 -0300

    feat: First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Post } from './Post'
+
+export function App() {
+  return (
+    <>
+      <Post 
+        author="Guilherme" 
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Quas quasi atque iure facilis illo labore nulla. 
+          Aspernatur sunt animi officiis vel consectetur? 
+          Numquam quam quaerat eum incidunt distinctio, a sapiente."
+      />
+      <Post 
+        author="Jobson"
+        content="Mto foda!!"
+      />
+    </>
+
+  )
+}
+
